4bed08f3b0e4f1523aa06acb103fed0ed789162e
sanjeevbrok99 <mishragsanjeev1999@gmail.com>
2023-11-07 21:25:46 +0530

    get book by Id and update book by id api creation done

--- src/controllers/book.controller.js
+++ src/controllers/book.controller.js
@@ -12,10 +12,42 @@ export const createBook = async (req, res) => {
     }
   };
 
+// Get a list of all books
+
 export const listAllBook =  async (req, res) => {
     try {
       const books = await Book.find();
       res.status(200).json(books);
+    } catch (error) {
+        console.log(error)
+      res.status(500).json({ error: 'Internal Server error' });
+    }
+  };
+
+  // Get details of a specific book by ID
+
+export const getBookById =  async (req, res) => {
+    try {
+      const book = await Book.findById(req.params.id);
+      if (!book) {
+        return res.status(404).json({ error: 'Book not found' });
+      }
+      res.status(200).json(book);
+    } catch (error) {
+        console.log(error)
+      res.status(500).json({ error: 'Internal Server error' });
+    }
+  };
+  
+// Update a book's details by ID
+
+ export const updateBook =  async (req, res) => {
+    try {
+      const book = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true });
+      if (!book) {
+        return res.status(404).json({ error: 'Book not found' });
+      }
+      res.status(200).json(book);
     } catch (error) {
         console.log(error)
       res.status(500).json({ error: 'Internal Server error' });
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,13 +1,18 @@
 import express from "express";
 import {
    createBook,
-   listAllBook
+   listAllBook,
+   getBookById,
+   updateBook
   } from "../controllers/book.controller.js";
 
   
   const router = express.Router();
   
   router.route('/create').post(createBook)
-  router.route('/getAll').post(createBook)
+  router.route('/getAll').get(createBook)
+  router.route('/getBook/:id').get(createBook)
+
+  router.route('/update/:id').put(createBook)
 
   export default router;
